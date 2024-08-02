9f498bfd5fb8659b78141d73538f10e8ca3610e2
Md. Raiyan Takrim <raiyan.takrim3@gmail.com>
2024-08-02 22:48:21 +0600

    Before add the edit functionality

---
+++ src/lib/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        title: "Reading Refactoring UI Book",
+        compleated: true
+    },
+    {
+        title: "Create TODO Application",
+        compleated: false
+    },
+    {
+        title: "Make A Call To The New Client",
+        compleated: false
+    },
+]
